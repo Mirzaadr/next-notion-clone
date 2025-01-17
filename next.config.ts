711a3fbcaac58994e9d2851d7714e9ec7236f48b
Mirzaadr <mirzaadr34@yahoo.com>
2025-01-17 10:12:27 +0700

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb", // Set desired value here
+    },
+  },
 };
 
 export default nextConfig;
